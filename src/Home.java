ad5250177e39793b447fee1df3568d3b33371a8c
Huisand18 <71809399+Huisand18@users.noreply.github.com>
2021-04-10 18:17:21 +0700

    Create Home.java

---
+++ src/Home.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.menu();
+        product.getDataValue();
+    }
+}
