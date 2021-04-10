d25027643ba92de190743d1c1231297ecc0e4052
Huisand18 <71809399+Huisand18@users.noreply.github.com>
2021-04-10 18:25:58 +0700

    Delete Input.java

--- src/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Input {
-    public static void main(String[] args) {
-        Product product = new Product();
-        product.menu();
-        product.getDataValue();
-    }
-}
